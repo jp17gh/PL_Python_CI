c92242661fb9385d43b700562712052ba30c747a
jp17gh <24933009+jp17gh@users.noreply.github.com>
2018-08-25 09:30:49 -0700

    removed the negative test case

--- app-tests.py
+++ app-tests.py
@@ -9,8 +9,5 @@ class MyTest(unittest.TestCase):
         self.assertEqual(app.my_function(0,0),0)
         self.assertEqual(app.my_function(1.1,1.1),2.2)
 
-    def test_negative_test(self):
-        self.assertEqual(app.my_function(5.5,3),-1)
-
 if __name__ == '__main__':
     unittest.main()
